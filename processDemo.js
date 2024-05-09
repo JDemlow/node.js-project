73e11e7aac53a6420d13a61175c35daeb7cd76e7
JDemlow <jdemlowdev@gmail.com>
2024-05-09 14:36:58 -0600

    finished demo files

---
+++ processDemo.js
@@ -0,0 +1,38 @@
+// argv
+
+console.log(process.argv);
+console.log(process.argv[2]);
+
+// process.env
+
+console.log(process.env.COMPUTERNAME);
+
+// pid
+
+console.log(process.pid);
+
+// cwd()
+
+console.log(process.cwd());
+
+// title
+
+console.log(process.title);
+
+//  memory usage
+
+console.log(process.memoryUsage());
+
+// uptime
+
+console.log(process.uptime());
+
+process.on("exit", (code) => {
+  console.log(`About to exit with code: ${code}`);
+});
+
+// exit
+
+process.exit(0);
+
+console.log("Hello from after exit");
